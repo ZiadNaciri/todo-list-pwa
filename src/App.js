de02ed9eadaa5b49cd3db6b9e3d09589ab9454c9
exaltedseraph <ziadnaciri.1@gmail.com>
2023-01-30 05:57:11 +0100

    fixed background and performance score

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import NetworkStatus from "./components/NetworkStatus";
+
+function App() {
+  return (
+    <div className="todo-app">
+      
+      <NetworkStatus />
+    
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
