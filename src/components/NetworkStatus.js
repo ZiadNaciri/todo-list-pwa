bf80bd3d331958104bea774c87bdcce4bb5b8eb5
exaltedseraph <ziadnaciri.1@gmail.com>
2023-02-21 19:01:50 +0100

    update notification status.

--- src/components/NetworkStatus.js
+++ src/components/NetworkStatus.js
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { BiWifi, BiWifiOff } from 'react-icons/bi';
+import { BiWifi, BiWifiOff } from "react-icons/bi";
+import toast from "react-hot-toast";
 import "../App.css";
+//import Notification from "./Notification";
 
 function NetworkStatus() {
-  
   const [isOnline, setIsOnline] = useState(navigator.onLine);
   const [offlineAt, setOfflineAt] = useState(null);
   useEffect(() => {
     function handleOnlineStatusChange() {
       if (navigator.onLine) {
-        //console.log(" internet online");
-        
         setIsOnline(true);
         setOfflineAt(null);
+        toast.success("You are back online!");
       } else {
-       // console.log(" internet offlinee");
-
         setIsOnline(false);
         setOfflineAt(new Date());
 
         // Show push notification
-        new Notification('Internet connection lost !!', {
-          body: 'You are now offline!'
-        });
+        /* new Notification("Internet connection lost !!", {
+          body: "You are now offline!",
+        }); */
+
+        toast.error("You lost internet connection!");
       }
     }
-        
+
     window.addEventListener("online", handleOnlineStatusChange);
     window.addEventListener("offline", handleOnlineStatusChange);
 
@@ -45,15 +45,16 @@ function NetworkStatus() {
 
   return (
     <div className="wifi-off">
-      <BiWifiOff /> You have been offline
-      since {new Date(offlineAt).toLocaleDateString("en-GB", {
-          day: "numeric",
-          month: "numeric",
-          year: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-          hour12: true,})}
+      <BiWifiOff /> You have been offline since{" "}
+      {new Date(offlineAt).toLocaleDateString("en-GB", {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+      })}
     </div>
   );
 }
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,36 +1,40 @@
-import React, {useState, useEffect} from 'react'
-import toast, { Toaster } from 'react-hot-toast';
-import { requestPermission, onMessageListener } from '../firebase';
+import React, { useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { requestPermission, onMessageListener } from "../firebase";
 
 const Notification = () => {
-  const [notification, setNotification] = useState({title: '', body: ''});
-  const notify = () =>  toast(<ToastDisplay/>);
+  const [notification, setNotification] = useState({ title: "", body: "" });
+  const notify = () => toast(<ToastDisplay />);
+
   function ToastDisplay() {
     return (
       <div>
-        <p><b>{notification?.title}</b></p>
+        <p>
+          <b>{notification?.title}</b>
+        </p>
         <p>{notification?.body}</p>
       </div>
     );
-  };
+  }
 
   useEffect(() => {
-    if (notification?.title ){
-     notify()
+    if (notification?.title) {
+      notify();
     }
-  }, [notification])
+  }, [notification]);
 
   requestPermission();
 
   onMessageListener()
     .then((payload) => {
-      setNotification({title: payload?.notification?.title, body: payload?.notification?.body});     
+      setNotification({
+        title: payload?.notification?.title,
+        body: payload?.notification?.body,
+      });
     })
-    .catch((err) => console.log('failed: ', err));
+    .catch((err) => console.log("failed: ", err));
 
-  return (
-     <Toaster/>
-  )
-}
+  return <Toaster ClassName="toaster" />;
+};
 
 export default Notification;
